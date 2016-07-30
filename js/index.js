7061ba5f75be75bca0142332c4ad8814d6bc0564
jianganglu <1055375563@qq.com>
2016-07-30 10:59:41 +0800

    Initial Commit.

---
+++ js/index.js
@@ -0,0 +1,41 @@
+﻿;(function($) {
+  'use strict';
+
+  $(function() {
+
+    var tmpl = null;
+
+    function init() {
+      getTmpl();
+      getData();
+    }
+
+    function getTmpl() {
+      $.ajax({
+        url: '../tmpl/tmpl.ejs',
+        type: 'GET',
+      }).done(function(resp) {
+        tmpl = _.template(resp); 
+      });
+    }
+
+    function getData() {
+      $.ajax({
+        url: '/json/data.json',
+        type: 'GET',
+        dataType: 'json',
+      }).done(function(resp) {
+        if(resp.code === 'ACK') {
+          $('#tmpl-wrap').html(tmpl({
+            data: resp.data
+          }));
+
+          $('#tmpl-wrap').append(_.template($('#tmpl').html())({data: resp.data}));
+        }
+      });
+    }
+
+    init();
+
+  });
+})(jQuery);
